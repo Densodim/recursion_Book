531f856fb231113b3ff450f4c99e36d2e3e15535
Densodim <25denso@gmail.com>
2025-01-06 00:03:08 +0300

    add permutations and nested loop permutations in Chapter - 6

---
+++ 1-Part/6-chapter/nestedLoopPermutations.js
@@ -0,0 +1,9 @@
+for (let a of ['A', 'B', 'C', 'D', 'E']) {
+    for (let b of ['A', 'B', 'C', 'D', 'E']) {
+        for (let c of ['A', 'B', 'C', 'D', 'E']) {
+            for (let d of ['A', 'B', 'C', 'D', 'E']) {
+                console.log(a, b, c, d)
+            }
+        }
+    }
+}
